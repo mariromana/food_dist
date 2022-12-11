336f41b594efd1109545243318ff90f3d415c9f6
mari <mariroman604@gmail.com>
2022-12-11 16:21:02 +0100

    addDefaultClasses

--- js/script.js
+++ js/script.js
@@ -169,41 +169,46 @@ function getZero(num) {
     // use classes for cards
 
     class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector);
             this.transfer = 27;
-            this.changeToUAH();
+            this.changeToUAH(); 
         }
 
         changeToUAH() {
-            this.price = +this.price * this.transfer;
-
+            this.price = this.price * this.transfer; 
         }
 
         render() {
-             const element = document.createElement('div');
-             element.innerHTML = `  
-              <div class="menu__item">
-             <img src=${this.scr} alt=${this.alt}>
-             <h3 class="menu__item-subtitle">${this.title}</h3>
-             <div class="menu__item-descr">${this.descr}</div>
-             <div class="menu__item-divider"></div>
-             <div class="menu__item-price">
-                 <div class="menu__item-cost">Цена:</div>
-                 <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-             </div>
-         </div>
-         `;
+            const element = document.createElement('div');
+
+            if (this.classes.length === 0) {
+                this.classes = "menu__item";
+                element.classList.add(this.classes);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
+            element.innerHTML = `
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            `;
             this.parent.append(element);
         }
-
-
     }
+
     new MenuCard(
         "img/tabs/vegy.jpg",
         "vegy",
@@ -232,9 +237,9 @@ function getZero(num) {
     ).render();
 
 
+
     // const div = new MenuCard();
     // div.render();
 });
-
 
 
